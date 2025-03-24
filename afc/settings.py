ae9fb3d828a38cb933d6eb8edebc8910229259d8
HabeebFF <habeebmuftau05@gmail.com>
2025-03-24 08:07:46 +0100

    added 1 allowed host

--- afc/settings.py
+++ afc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-al*a1qpu75zbfu!p7aki6)(p34yc=6(r18uhc)#zxv9oo!pi2)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://afc.pythonanywhere.com/", "afc.pythonanywhere.com"]
 
 
 MEDIA_URL = '/media/'
