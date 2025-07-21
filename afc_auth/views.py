44906cd540c89411e99c0a424d26f75a455bdc38
HabeebFF <habeebmuftau05@gmail.com>
2025-07-21 11:19:54 +0100

    fixed edit profile

--- afc_auth/views.py
+++ afc_auth/views.py
@@ -577,7 +577,12 @@ def edit_profile(request):
     session_token = request.headers.get("Authorization")
 
     if not session_token:
-        return Response({"message": "Session token is required."}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'status': 'error', 'message': 'Authorization header is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not session_token.startswith("Bearer "):
+        return Response({'status': 'error', 'message': 'Invalid token format'}, status=status.HTTP_400_BAD_REQUEST)
+
+    session_token = session_token.split(" ")[1]
 
     # Identify the logged-in user using the session token
     try:
@@ -646,3 +651,5 @@ def get_user_profile(request):
         "team": user.team.team_name if hasattr(user, 'team') else None,
         "role": user.role,
     }, status=status.HTTP_200_OK)
+
+
