86bb53d460beab25a4e824c2ee32242c05e27fdf
HabeebFF <habeebmuftau05@gmail.com>
2025-09-23 17:33:29 +0100

    added delete role url

--- afc_auth/urls.py
+++ afc_auth/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('get-all-news/', get_all_news, name='get_all_news'),
     path('delete-news/', delete_news, name='delete_news'),
     path('add-role/', add_role, name='add_role'),
+    path('delete-role/', delete_role, name='delete_role'),
     path('get-admin-info/', get_admin_info, name='get_admin_info'),
     path('get-all-roles/', get_all_roles, name='get_all_roles'),
     path('get-all-user-and-user-roles/', get_all_user_and_user_roles, name='get_all_user_and_user_roles'),
