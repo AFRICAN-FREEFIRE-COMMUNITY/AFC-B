38547eae5b0d7c79be4a2442aa1850f3d76252b6
HabeebFF <habeebmuftau05@gmail.com>
2025-11-22 15:42:02 +0100

    push something

--- afc/settings.py
+++ afc/settings.py
@@ -200,3 +200,12 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
