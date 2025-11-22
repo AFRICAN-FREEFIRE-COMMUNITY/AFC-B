dab98ccbbd7fa7e12ea37251de4efb542ad45c93
HabeebFF <habeebmuftau05@gmail.com>
2025-11-22 18:26:56 +0100

    CHANGED PS

--- afc_auth/views.py
+++ afc_auth/views.py
@@ -57,8 +57,9 @@ def send_email(to_address, subject, html_body):
     # Gmail SMTP server credentials
     smtp_server = 'smtp.gmail.com'
     smtp_port = 465  # or 587 for TLS
-    from_address = 'africanfreefirecommunity1@gmail.com' #vermillioninformation@gmail.com, Info@v-ent.co
-    password = 'rvgn rzha ihli dfdp'  # Or your actual Gmail password (if less secure apps are enabled)
+    from_address = 'africanfreefirecommunity2@gmail.com' #vermillioninformation@gmail.com, Info@v-ent.co
+    password = 'yyzm prff sjfo bcmg'
+# 'rvgn rzha ihli dfdp'  # Or your actual Gmail password (if less secure apps are enabled)
 
     try:
         # Create a MIMEMultipart email object
