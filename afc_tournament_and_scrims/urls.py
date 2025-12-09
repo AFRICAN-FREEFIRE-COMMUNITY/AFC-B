727f84cab01b8b9007956b5293caf1160cc0c405
HabeebFF <habeebmuftau05@gmail.com>
2025-12-09 20:53:42 +0100

    modified

--- afc_tournament_and_scrims/urls.py
+++ afc_tournament_and_scrims/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path("admin/", admin.site.urls),
     # path('admin-login/', admin_login, name='admin_login'),
     path('create-event/', create_event, name='create_event'),
-    path('edit-event', edit_event, name='edit_event'),
+    path('edit-event/', edit_event, name='edit_event'),
     path('create-leaderboard/', create_leaderboard, name='create_leaderboard'),
     path('get-all-events/', get_all_events, name='get_all_events'),
     path('get-event-details/', get_event_details, name='get_event_details'),
