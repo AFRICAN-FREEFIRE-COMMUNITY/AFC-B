27c8ed0c8f7324243ac14723ade979b9570cd21a
HabeebFF <habeebmuftau05@gmail.com>
2025-12-17 15:50:47 +0100

    test

--- afc_auth/views.py
+++ afc_auth/views.py
@@ -169,7 +169,7 @@ def login(request):
         user.save()
 
         ip = get_client_ip(request)
-        geo = lookup_ip(ip)
+        geo = lookup_ip("8.8.8.8")  # Replace with 'ip' for real IP lookup
 
         if geo:
             print(geo["country_code"], geo["country"])
