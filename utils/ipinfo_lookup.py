7fafeb1e7c4eea12f9b9859283567db288491d66
HabeebFF <habeebmuftau05@gmail.com>
2025-12-17 15:50:47 +0100

    test

--- utils/ipinfo_lookup.py
+++ utils/ipinfo_lookup.py
@@ -1,27 +1,17 @@
 import geoip2.database
-import os
 
-# Path to your MMDB file
 MMDB_PATH = "/home/ubuntu/ipinfo/ipinfo_lite.mmdb"
-
 reader = geoip2.database.Reader(MMDB_PATH)
 
 def lookup_ip(ip):
-    """
-    Returns country, country_code, and continent from an IP
-    """
     try:
         response = reader.city(ip)
         return {
-            "response": response,
+            "country": response.country.name,
+            "country_code": response.country.iso_code,
+            "continent": response.continent.name
         }
-        # return {
-        #     "country": response.country.name,
-        #     "country_code": response.country.iso_code,
-        #     "continent": response.continent.name
-        # }
     except geoip2.errors.AddressNotFoundError:
-        # IP not in database
         return None
     except Exception as e:
         print("IP lookup error:", e)
