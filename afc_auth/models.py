52908591360547548f7cc17ddae8d7d8ef63eb07
HabeebFF <habeebmuftau05@gmail.com>
2026-01-24 23:48:52 +0100

    addedd

--- afc_auth/models.py
+++ afc_auth/models.py
@@ -206,7 +206,7 @@ class News(models.Model):
     ]
 
     news_id = models.AutoField(primary_key=True)
-    slug = models.SlugField(max_length=220, unique=True, blank=True, db_index=True)
+    slug = models.SlugField(max_length=220, unique=True, blank=True, db_index=True, null=True)
     news_title = models.CharField(max_length=255)
     content = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
