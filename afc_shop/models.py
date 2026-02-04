71a281e20ab58887a49881c2e2cc7412c2937977
HabeebFF <habeebmuftau05@gmail.com>
2026-02-04 15:12:01 +0100

    fixed bug

--- afc_shop/models.py
+++ afc_shop/models.py
@@ -398,6 +398,8 @@ class Order(models.Model):
     state = models.CharField(max_length=50, blank=True)
     postcode = models.CharField(max_length=20, blank=True)
 
+    tax = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
+
 
     def __str__(self):
         return f"Order #{self.id} - {self.user.username} - {self.status}"
