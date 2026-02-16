b19e9be10f9ec9fba3809fd4149cb511ff872679
HabeebFF <habeebmuftau05@gmail.com>
2026-02-17 00:59:40 +0100

    fixed bug

--- afc_shop/views.py
+++ afc_shop/views.py
@@ -1251,7 +1251,7 @@ def buy_now(request):
             discount_total=total_discount,
             total=grand_total,
             status="pending",
-            coupon_code=None,  # item-level coupons
+            coupon_code=applied_coupon.code if applied_coupon else None,  # item-level coupons
             first_name=request.data.get("first_name"),
             last_name=request.data.get("last_name"),
             email=request.data.get("email"),
