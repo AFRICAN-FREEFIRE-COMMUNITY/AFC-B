f112db170ed4d149245fd7a3d57558c97c6ffb4a
HabeebFF <habeebmuftau05@gmail.com>
2026-03-13 15:53:11 +0100

    fixed bug

--- afc_tournament_and_scrims/views.py
+++ afc_tournament_and_scrims/views.py
@@ -13603,7 +13603,7 @@ def add_teams_to_event(request):
             ))
 
         # add the members of the team to TournamentTeamMember if they are not already there
-        for member in team.members.select_related("user").all():
+        for member in team.teammembers.select_related("user").all():
             if not TournamentTeamMember.objects.filter(tournament_team__event=event, tournament_team__team=team, user=member.user).exists():
                 TournamentTeamMember.objects.create(
                     tournament_team=new_tournament_teams[-1],  # reference the newly created TournamentTeam
