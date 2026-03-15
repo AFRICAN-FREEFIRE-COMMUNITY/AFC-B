cebe6ec67de3e0786554b9655b21fe8028ae4ef8
HabeebFF <habeebmuftau05@gmail.com>
2026-03-15 16:09:27 +0100

    fixed bug

--- afc_tournament_and_scrims/views.py
+++ afc_tournament_and_scrims/views.py
@@ -2135,10 +2135,10 @@ def get_event_details(request):
             ).exists()
 
     # -------- SLOT LEFT --------
-    total_registered = RegisteredCompetitors(event=event).all()
-    total_slots= event.max_teams_or_players
+    # total_registered = RegisteredCompetitors(event=event).all()
+    # total_slots= event.max_teams_or_players
 
-    slots_left = total_slots - total_registered
+    # slots_left = total_slots - total_registered
 
 
     sponsors = SponsorEvent.objects.filter(event=event).select_related("sponsor")
@@ -2188,7 +2188,7 @@ def get_event_details(request):
         "is_waitlist enabled": event.is_waitlist_enabled,
         "waitlist_capacity": event.waitlist_capacity,
         "waitlist discord_ role_id": event.waitlist_discord_role_id,
-        "slots_left": slots_left,
+        # "slots_left": slots_left,
     }
 
     # ============================================================
