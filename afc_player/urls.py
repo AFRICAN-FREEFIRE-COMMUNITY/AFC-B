0108bfe32decb6587fd8275830379f74aa53d313
HabeebFF <habeebmuftau05@gmail.com>
2026-03-27 02:23:16 +0100

    added Get ALL USERS

--- afc_player/urls.py
+++ afc_player/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("get-player-details/", get_player_details, name="get_player_details"),
+    path("get-all-players/", get_all_users, name="get_all_users"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
